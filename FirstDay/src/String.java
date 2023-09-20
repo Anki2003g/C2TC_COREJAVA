cd27141cc08df66ea4f3b2c8a96075b772825488
Ankitha <Ankitha@Ankitha>
2023-09-20 21:15:30 +0530

    First

---
+++ FirstDay/src/String.java
@@ -0,0 +1,11 @@
+
+public class String {
+
+	public static void main(java.lang.String[] args) 
+	{
+		String var= new String();
+		System.out.println(var);// TODO Auto-generated method stub
+
+	}
+
+}
